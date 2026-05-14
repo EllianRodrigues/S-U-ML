7cb53aa1e67836f634b6cf7fa6ba3431b2ce1fe7
Ellian <ellian.rodrigues.ti@gmail.com>
2026-05-14 12:35:06 -0300

    feat: add agent UseCase

---
+++ agents/UseCase/UseCase.py
@@ -0,0 +1,18 @@
+from models.requirement_extraction.Qwen2_5_5B import Qwen25_5B
+import tomli as tomllib
+
+with open("./config.toml", "rb") as file:
+    text = tomllib.load(file)["use_case"]["input_text"]
+
+def run_use_case(text: str = text, model_cls=Qwen25_5B):
+	model = model_cls()
+
+	print(f"\nInput Text:\n{text}\n")
+
+	try:
+		use_case = model.extract_use_case(text)
+		print(f"Extracted Use Case:\n{use_case}")
+		return use_case
+	except Exception as e:
+		print(f"Error extracting use case: {e}")
+		return None
--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from models.requirement_extraction.Qwen2_5_5B import Qwen25_5B
-import tomli as tomllib
-
-with open("config.toml", "rb") as file:
-    text = tomllib.load(file)["use_case"]["input_text"]
+from agents.UseCase.UseCase import run_use_case
 
 def main():
 
-    model = Qwen25_5B()
-
-    print(f"\nInput Text:\n{text}\n")
-
-    try:
-        use_case = model.extract_use_case(text)
-        print(f"Extracted Use Case:\n{use_case}")
-    except Exception as e:
-        print(f"Error extracting use case: {e}")
+    run_use_case()
 
 if __name__ == "__main__":
     main()
