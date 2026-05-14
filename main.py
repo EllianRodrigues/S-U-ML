88fdbc9968534b44ba7b1516ea281364e683966e
Ellian <ellian.rodrigues.ti@gmail.com>
2026-05-14 16:57:44 -0300

    feat: fiz some bugs and add full pipiline without comparasion

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from agents.UseCase.UseCase import run_use_case
 from agents.UML.UML import run_uml
+from agents.UML_To_UseCase.UML_To_UseCase import run_uml_to_use_case
 
 def main():
 
     use_case = run_use_case()
-    run_uml(use_case)
+    uml = run_uml(use_case)
+    run_uml_to_use_case(uml)
 
 if __name__ == "__main__":
     main()
